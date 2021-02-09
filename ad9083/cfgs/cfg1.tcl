6b0aeceeeaf186dcf20cb90955a89e35d27fa129
Sergiu Arpadi <sergiu.arpadi@analog.com>
2021-02-09 11:54:49 +0000

    ad9083: Add testbench

---
+++ ad9083/cfgs/cfg1.tcl
@@ -0,0 +1,24 @@
+global ad_project_params
+
+set ad_project_params(JESD_MODE) 8B10B
+
+set ad_project_params(REF_CLK_RATE) 500
+
+set ad_project_params(RX_RATE) 10
+set ad_project_params(TX_RATE) 10
+
+set ad_project_params(RX_NUM_LINKS) 1
+set ad_project_params(RX_JESD_M) 16
+set ad_project_params(RX_JESD_L) 4
+set ad_project_params(RX_JESD_S) 1
+set ad_project_params(RX_JESD_NP) 16
+set ad_project_params(RX_JESD_F) 8
+set ad_project_params(RX_JESD_K) 32
+
+set ad_project_params(TX_NUM_LINKS) 1
+set ad_project_params(TX_JESD_M) 16
+set ad_project_params(TX_JESD_L) 4
+set ad_project_params(TX_JESD_S) 1
+set ad_project_params(TX_JESD_NP) 16
+set ad_project_params(TX_JESD_F) 8
+set ad_project_params(TX_JESD_K) 32

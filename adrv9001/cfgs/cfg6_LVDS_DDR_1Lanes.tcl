ba950b1da53db541e1d3369c4f7f0de0b65a21f9
Laszlo Nagy <laszlo.nagy@analog.com>
2021-02-12 13:16:59 +0000

    adrv9001: Add cfg file for all supported modes

---
+++ adrv9001/cfgs/cfg6_LVDS_DDR_1Lanes.tcl
@@ -0,0 +1,7 @@
+global ad_project_params
+
+set ad_project_params(CMOS_LVDS_N) 0
+set ad_project_params(SDR_DDR_N) 0
+set ad_project_params(SINGLE_LANE) 1
+set ad_project_params(USE_RX_CLK_FOR_TX) 0
+

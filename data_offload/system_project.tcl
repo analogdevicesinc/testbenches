bbcbfdd689adb10937a017f7f7d4ee1e001152d9
Istvan Csomortani <istvan.csomortani@analog.com>
2021-02-23 13:07:03 +0000

    tb/data_offload: Initial commit

---
+++ data_offload/system_project.tcl
@@ -0,0 +1,51 @@
+
+source ../scripts/adi_sim.tcl
+source ../../library/scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+if {$argc < 1} {
+  puts "Expecting at least one argument that specifies the test configuration"
+  set cfg_file cfg2.tcl
+  #exit 1
+} else {
+  set cfg_file [lindex $argv 0]
+}
+
+# Read common configuration file
+source "cfgs/common_cfg.tcl"
+# Read configuration file with topology information
+source "cfgs/${cfg_file}"
+
+# Set the project name
+set project_name [file rootname $cfg_file]
+
+# Create the project
+#set bd_design_name "test_harness"
+set part "xc7z045ffg900-2"
+
+adi_sim_project_xilinx $project_name $part
+
+# Add test files to the project
+adi_sim_project_files [list \
+ "../common/sv/utils.svh" \
+ "../common/sv/logger_pkg.sv" \
+ "../common/sv/reg_accessor.sv" \
+ "../common/sv/m_axis_sequencer.sv" \
+ "../common/sv/s_axis_sequencer.sv" \
+ "../common/sv/m_axi_sequencer.sv" \
+ "../common/sv/s_axi_sequencer.sv" \
+ "adi_regmap.sv" \
+ "adi_regmap_doffload.sv" \
+ "do_scoreboard.sv" \
+ "environment.sv" \
+ "tests/test_program.sv" \
+ "system_tb.sv" \
+]
+
+#set a default test program
+adi_sim_add_define "TEST_PROGRAM=test_program"
+
+adi_sim_generate $project_name
+
+#launch_simulation
+#run all
---
+++ data_offload/system_tb.sv
@@ -0,0 +1,9 @@
+`include "utils.svh"
+
+module system_tb();
+
+    test_program test();
+    test_harness `TH ();
+
+
+endmodule

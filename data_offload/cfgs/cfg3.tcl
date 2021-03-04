c57d605ebce67004873b5e19ccdd9be904dc81a8
Istvan Csomortani <istvan.csomortani@analog.com>
2021-03-04 13:02:38 +0000

    data_offload: Make the DDR related parameters independent from the data path type

--- data_offload/cfgs/cfg3.tcl
+++ data_offload/cfgs/cfg3.tcl
@@ -1,16 +1,20 @@
 global ad_project_params
 
+set ad_project_params(ADC_DATA_PATH_WIDTH)   8     ; ## NOTE: Must be ADC_OFFLOAD_SRC_WIDTH/8
+set ad_project_params(DAC_DATA_PATH_WIDTH)  16     ; ## NOTE: Must be DAC_OFFLOAD_DST_WIDTH/8
+
 set ad_project_params(ADC_PATH_TYPE) 0             ; ## RX
-set ad_project_params(ADC_OFFLOAD_MEM_TYPE) 1      ; ## External storage
-set ad_project_params(ADC_OFFLOAD_SIZE) 4294967295 ; ## Storage size in bytes
-set ad_project_params(ADC_OFFLOAD_SRC_DWIDTH) 128  ; ## Source data width
-set ad_project_params(ADC_OFFLOAD_DST_DWIDTH) 64  ; ## Destination data width
-set ad_project_params(ADC_DDR_DATA_WIDTH) 512      ; ## PLDDR's AXI4 interface data width
-set ad_project_params(ADC_DDR_ADDRESS_WIDTH) 28    ; ## PLDDR's AXI4 interface address width
+set ad_project_params(ADC_OFFLOAD_MEM_TYPE) 0      ; ## External storage
+set ad_project_params(ADC_OFFLOAD_SIZE) 2048       ; ## Storage size in bytes
+set ad_project_params(ADC_OFFLOAD_SRC_DWIDTH) 64   ; ## Source data width
+set ad_project_params(ADC_OFFLOAD_DST_DWIDTH) 64   ; ## Destination data width
 
 set ad_project_params(DAC_PATH_TYPE) 0             ; ## TX
-set ad_project_params(DAC_OFFLOAD_MEM_TYPE) 0      ; ## External storage
+set ad_project_params(DAC_OFFLOAD_MEM_TYPE) 1      ; ## External storage
 set ad_project_params(DAC_OFFLOAD_SIZE) 2048       ; ## Storage size in bytes
-set ad_project_params(DAC_OFFLOAD_SRC_DWIDTH) 128  ; ## Source data width
+set ad_project_params(DAC_OFFLOAD_SRC_DWIDTH) 64   ; ## Source data width
 set ad_project_params(DAC_OFFLOAD_DST_DWIDTH) 128  ; ## Destination data width
+
+set ad_project_params(PLDDR_OFFLOAD_DATA_WIDTH) 512      ; ## PLDDR's AXI4 interface data width
+set ad_project_params(PLDDR_OFFLOAD_ADDRESS_WIDTH) 28    ; ## PLDDR's AXI4 interface address width
 

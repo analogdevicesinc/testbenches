689618fddd3ace0f6d1faf0b662a6bf60c6eb7b8
David Winter <david.winter@analog.com>
2021-04-29 19:42:00 +0200

    data_offload_2: Add test_program_sync to test hardware sync
    Signed-off-by: David Winter <david.winter@analog.com>

--- data_offload_2/system_project.tcl
+++ data_offload_2/system_project.tcl
@@ -24,6 +24,8 @@ set part "xczu9eg-ffvb1156-2-e"
 
 adi_sim_project_xilinx $project_name $part
 
+set TST $::env(TST)
+
 # Add test files to the project
 adi_sim_project_files [list \
  "../common/sv/utils.svh" \
@@ -34,16 +36,13 @@ adi_sim_project_files [list \
  "../common/sv/m_axi_sequencer.sv" \
  "../common/sv/s_axi_sequencer.sv" \
  "environment.sv" \
- "tests/test_program.sv" \
+ "tests/$TST.sv" \
  "do_scoreboard.sv" \
  "system_tb.sv" \
  "data_offload_pkg.sv" \
 ]
 
 #set a default test program
-adi_sim_add_define "TEST_PROGRAM=test_program"
+adi_sim_add_define "TEST_PROGRAM=$TST"
 
 adi_sim_generate $project_name
-
-#launch_simulation
-#run all

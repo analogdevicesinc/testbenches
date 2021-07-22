17032700d42a7071b6089ff332b8a6ddc834e7f5
Laszlo Nagy <laszlo.nagy@analog.com>
2021-07-22 11:47:29 +0100

    jesd_loopback: Support tests with M=32 F=64

---
+++ jesd_loopback/cfgs/cfg6_F64.tcl
@@ -0,0 +1,13 @@
+source [file dirname [file normalize [info script]]]/common.tcl
+
+set ad_project_params(LINK_MODE) $JESD_8B10B
+
+set ad_project_params(JESD_M) 32
+set ad_project_params(JESD_L) 1
+set ad_project_params(JESD_F) 64
+set ad_project_params(JESD_K) 32
+set ad_project_params(JESD_S) 1
+set ad_project_params(JESD_NP) 16
+
+set ad_project_params(REF_CLK_RATE) 250
+set ad_project_params(LANE_RATE) 10

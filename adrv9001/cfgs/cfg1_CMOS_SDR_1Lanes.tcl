ecfc5e3b37b98918017062bd75e128d8292471a8
alin724 <sferle.do.alin@utcluj.didatec.ro>
2021-07-27 12:23:20 +0300

    adrv9001: Add cfg files for symbol operation supported modes

--- adrv9001/cfgs/cfg1_CMOS_SDR_1Lanes.tcl
+++ adrv9001/cfgs/cfg1_CMOS_SDR_1Lanes.tcl
@@ -4,4 +4,5 @@ set ad_project_params(CMOS_LVDS_N) 1
 set ad_project_params(SDR_DDR_N) 1
 set ad_project_params(SINGLE_LANE) 1
 set ad_project_params(USE_RX_CLK_FOR_TX) 0
-
+set ad_project_params(SYMB_OP) 0
+set ad_project_params(SYMB_8_16B) 0

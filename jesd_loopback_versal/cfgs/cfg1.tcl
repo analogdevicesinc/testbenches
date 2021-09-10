e00009e998e43f215f5f24491187d0c2c2e4491e
Laszlo Nagy <laszlo.nagy@analog.com>
2021-09-10 10:02:02 +0100

    jesd_loopback_versal: Upadte ref clock to 375 MHz

--- jesd_loopback_versal/cfgs/cfg1.tcl
+++ jesd_loopback_versal/cfgs/cfg1.tcl
@@ -6,7 +6,7 @@ set ad_project_params(JESD_K) 256
 set ad_project_params(JESD_NP) 12
 
 set ad_project_params(LANE_RATE) 24.750
-set ad_project_params(REF_CLK_RATE) 200
+set ad_project_params(REF_CLK_RATE) 375
 #   [RX/TX]_PLL_SEL :  Used PLL in the Xilinx PHY used in 64B66B mode
 #                      Encoding is:
 #                         0 - CPLL

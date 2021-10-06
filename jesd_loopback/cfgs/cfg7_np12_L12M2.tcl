dd03ccf761ef7d8f0b38dbde2da565b119ad76b6
Laszlo Nagy <laszlo.nagy@analog.com>
2021-10-06 14:46:29 +0100

    jesd_loopback: Add config file with 12 lanes

---
+++ jesd_loopback/cfgs/cfg7_np12_L12M2.tcl
@@ -0,0 +1,13 @@
+source [file dirname [file normalize [info script]]]/common.tcl
+
+set ad_project_params(LINK_MODE) $JESD_8B10B
+
+set ad_project_params(JESD_L) 12
+set ad_project_params(JESD_M) 2
+set ad_project_params(JESD_F) 1
+set ad_project_params(JESD_K) 32
+set ad_project_params(JESD_S) 4
+set ad_project_params(JESD_NP) 12
+
+set ad_project_params(REF_CLK_RATE) 250
+set ad_project_params(LANE_RATE) 10

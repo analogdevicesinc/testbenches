4eb34e225a6062f3417a776d2e2d8890c42287e4
Laszlo Nagy <laszlo.nagy@analog.com>
2022-01-10 12:48:11 +0000

    adrv9001: cfg7_LVDS_DDR_2Lanes_noDDS: set LVDS

--- adrv9001/cfgs/cfg7_LVDS_DDR_2Lanes_noDDS.tcl
+++ adrv9001/cfgs/cfg7_LVDS_DDR_2Lanes_noDDS.tcl
@@ -1,6 +1,6 @@
 global ad_project_params
 
-set ad_project_params(CMOS_LVDS_N) 1
+set ad_project_params(CMOS_LVDS_N) 0
 set ad_project_params(SDR_DDR_N) 0
 set ad_project_params(SINGLE_LANE) 0
 set ad_project_params(USE_RX_CLK_FOR_TX) 0

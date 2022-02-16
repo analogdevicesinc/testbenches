26626e6f0907a88ef89e7bdf93ab03a83c071407
Laszlo Nagy <laszlo.nagy@analog.com>
2022-02-16 09:41:26 +0000

    hbm: Basic test structure without functional testing
    Initial framework to test HBM with third party sim however test default
    to AXI VIP to avoid failure.

---
+++ hbm/cfgs/cfg1.tcl
@@ -0,0 +1,8 @@
+global ad_project_params
+
+set ad_project_params(DATA_PATH_WIDTH) 16             ; ## 16 bytes
+
+set ad_project_params(SRC_CLOCK_FREQ) 250000000       ; ## Source clock frequency in Hz
+set ad_project_params(DST_CLOCK_FREQ) 300000000       ; ## Destination clock frequency in Hz
+
+

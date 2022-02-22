356a55374265390b22eefeaf3c7ca3beeaf7fcc2
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2022-02-22 08:12:09 +0000

    common: dmac_api: Updated the constructor and tasks

--- common/sv/dmac_api.sv
+++ common/sv/dmac_api.sv
@@ -38,38 +38,97 @@
 package dmac_api_pkg;
 
   import logger_pkg::*;
+  import adi_peripheral_pkg::*;
   import adi_regmap_dmac_pkg::*;
   import adi_regmap_pkg::*;
   import reg_accessor_pkg::*;
   import dma_trans_pkg::*;
   import axi_dmac_pkg::*;
 
-  class dmac_api;
+  class dmac_api extends adi_peripheral;
 
-    reg_accessor mng;
-
-    int DMAC_BA;
     // DMAC parameters
     axi_dmac_params_t p;
 
-    function new(reg_accessor mng,
-                 int DMAC_BA,
-                 axi_dmac_params_t p);
-      this.mng = mng;
-      this.DMAC_BA = DMAC_BA;
-      this.p = p;
+    // -----------------
+    //
+    // -----------------
+    function new(string name, reg_accessor bus, bit [31:0] base_address);
+      super.new(name, bus, base_address);
     endfunction
 
+    // -----------------
+    //
+    // -----------------
+    // Discover HW parameters                                                                 
+    task discover_params();                                                                   
+      bit [31:0] val;
+      bit  [3:0] bpb_dest_log2, bpb_src_log2, bpb_width_log2;
+      this.bus.RegRead32(this.base_address + GetAddrs(DMAC_PERIPHERAL_ID), val);              
+      p.ID = `GET_DMAC_PERIPHERAL_ID_PERIPHERAL_ID(val);
+      this.bus.RegRead32(this.base_address + GetAddrs(DMAC_INTERFACE_DESCRIPTION), val);      
+      bpb_dest_log2 = `GET_DMAC_INTERFACE_DESCRIPTION_BYTES_PER_BEAT_DEST_LOG2(val);          
+      p.DMA_DATA_WIDTH_DEST = (2**bpb_dest_log2)*8;
+      p.DMA_TYPE_DEST = `GET_DMAC_INTERFACE_DESCRIPTION_DMA_TYPE_DEST(val);
+      bpb_src_log2 = `GET_DMAC_INTERFACE_DESCRIPTION_BYTES_PER_BEAT_SRC_LOG2(val);            
+      p.DMA_DATA_WIDTH_SRC = (2**bpb_src_log2)*8;
+      p.DMA_TYPE_SRC = `GET_DMAC_INTERFACE_DESCRIPTION_DMA_TYPE_SRC(val);
+      bpb_width_log2 = `GET_DMAC_INTERFACE_DESCRIPTION_BYTES_PER_BURST_WIDTH(val);            
+      p.MAX_BYTES_PER_BURST = 2**bpb_width_log2;
+      p.DMA_2D_TRANSFER = `GET_DMAC_INTERFACE_DESCRIPTION_DMA_2D_TRANSFER(val);               
+    endtask : discover_params
+
+    // -----------------
+    //
+    // -----------------
+    task probe ();
+      super.probe();
+      discover_params();
+      `INFO(("Found %0s destination interface of %0d bit data width\n\t\tFound %0s source interface of %0d bit data width" ,
+        p.DMA_TYPE_DEST == 0 ? "AXI MemoryMap" : p.DMA_TYPE_DEST == 1 ? "AXI Stream" : p.DMA_TYPE_DEST == 2 ? "FIFO" : "Unknown",                                                             
+        p.DMA_DATA_WIDTH_DEST,
+        p.DMA_TYPE_SRC == 0 ? "AXI MemoryMap" : p.DMA_TYPE_SRC == 1 ? "AXI Stream" : p.DMA_TYPE_SRC == 2 ? "FIFO" : "Unknown",                                                                
+        p.DMA_DATA_WIDTH_SRC));
+      `INFO(("Found %0d max bytes per burst" , p.MAX_BYTES_PER_BURST));
+      `INFO(("Enabled support for 2D transfers: %0d" , p.DMA_2D_TRANSFER));
+    endtask : probe
+
+    // -----------------
+    //
+    // -----------------
+    function axi_dmac_params_t get_params();
+      return this.p;
+    endfunction : get_params
+
+    // -----------------
+    //
+    // -----------------
     task enable_dma();
-      mng.RegWrite32( DMAC_BA + GetAddrs(dmac_CONTROL),
-                                SetField(dmac_CONTROL, "ENABLE", 1));
-    endtask
+      this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_CONTROL),
+                                SetField(DMAC_CONTROL, "ENABLE", 1));
+    endtask : enable_dma
 
+    // -----------------
+    //
+    // -----------------
     task disable_dma();
-      mng.RegWrite32( DMAC_BA + GetAddrs(dmac_CONTROL),
-                                SetField(dmac_CONTROL, "ENABLE", 0));
-    endtask
+      this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_CONTROL),
+                                SetField(DMAC_CONTROL, "ENABLE", 0));
+    endtask : disable_dma
 
+    // -----------------
+    //
+    // -----------------
+    task set_flags(input bit[2:0] flags);
+      this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_FLAGS),
+                                SetField(DMAC_FLAGS, "CYCLIC", flags[0])|
+                                SetField(DMAC_FLAGS, "TLAST", flags[1])|
+                                SetField(DMAC_FLAGS, "PARTIAL_REPORTING_EN", flags[2]));
+    endtask : set_flags
+
+    // -----------------
+    //
+    // -----------------
     task wait_transfer_submission;
       bit [31:0] regData = 'h0;
       bit timeout;
@@ -79,8 +138,8 @@ package dmac_api_pkg;
       fork
         begin
           do
-            mng.RegRead32( DMAC_BA + GetAddrs(dmac_TRANSFER_SUBMIT), regData);
-          while (GetField(dmac_TRANSFER_SUBMIT,
+            this.bus.RegRead32( this.base_address + GetAddrs(DMAC_TRANSFER_SUBMIT), regData);
+          while (GetField(DMAC_TRANSFER_SUBMIT,
                           "TRANSFER_SUBMIT",
                           regData) != 0);
           `INFO(("Ready for submission "));
@@ -93,14 +152,20 @@ package dmac_api_pkg;
       if (timeout) begin
          `ERROR(("Waiting transfer submission TIMEOUT !!!"));
       end
-    endtask
+    endtask : wait_transfer_submission
 
+    // -----------------
+    //
+    // -----------------
     task transfer_start;
-      mng.RegWrite32( DMAC_BA + GetAddrs(dmac_TRANSFER_SUBMIT),
-                                SetField(dmac_TRANSFER_SUBMIT, "TRANSFER_SUBMIT", 1));
+      this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_TRANSFER_SUBMIT),
+                                SetField(DMAC_TRANSFER_SUBMIT, "TRANSFER_SUBMIT", 1));
       `INFO(("Transfer start"));
-    endtask
+    endtask : transfer_start
 
+    // -----------------
+    //
+    // -----------------
     task wait_transfer_done(input bit [3:0] transfer_id,
                             input bit partial_segment = 0,
                             input int segment_length = 0,
@@ -115,11 +180,11 @@ package dmac_api_pkg;
       fork
         begin
           while (~regData[transfer_id]) begin
-            mng.RegRead32( DMAC_BA + GetAddrs(dmac_TRANSFER_DONE), regData);
+            this.bus.RegRead32( this.base_address + GetAddrs(DMAC_TRANSFER_DONE), regData);
           end
           `INFO(("Transfer id %0d DONE",transfer_id));
 
-          partial_info_available = GetField(dmac_TRANSFER_DONE,
+          partial_info_available = GetField(DMAC_TRANSFER_DONE,
                                             "PARTIAL_TRANSFER_DONE",
                                             regData);
 
@@ -129,8 +194,8 @@ package dmac_api_pkg;
             end
 
             `INFO(("Found partial data info for ID  %0d",transfer_id));
-            mng.RegRead32( DMAC_BA + GetAddrs(dmac_PARTIAL_TRANSFER_LENGTH), regData);
-            segment_length_found = GetField(dmac_PARTIAL_TRANSFER_LENGTH,
+            this.bus.RegRead32( this.base_address + GetAddrs(DMAC_PARTIAL_TRANSFER_LENGTH), regData);
+            segment_length_found = GetField(DMAC_PARTIAL_TRANSFER_LENGTH,
                                             "PARTIAL_LENGTH",
                                              regData);
             if (segment_length_found != segment_length) begin
@@ -139,8 +204,8 @@ package dmac_api_pkg;
             end else begin
               `INFO(("Found partial data info length is %0d",segment_length));
             end
-            mng.RegRead32( DMAC_BA + GetAddrs(dmac_PARTIAL_TRANSFER_ID), regData);
-            id_found = GetField(dmac_PARTIAL_TRANSFER_ID,
+            this.bus.RegRead32( this.base_address + GetAddrs(DMAC_PARTIAL_TRANSFER_ID), regData);
+            id_found = GetField(DMAC_PARTIAL_TRANSFER_ID,
                                 "PARTIAL_TRANSFER_ID",
                                 regData);
 
@@ -161,21 +226,25 @@ package dmac_api_pkg;
       if (timeout) begin
          `ERROR(("Waiting transfer done TIMEOUT !!!"));
       end
-    endtask
+    endtask : wait_transfer_done
 
+    // -----------------
+    //
+    // -----------------
     task transfer_id_get(output bit [3:0] transfer_id);
-      mng.RegRead32( DMAC_BA + GetAddrs(dmac_TRANSFER_ID), transfer_id);
+      this.bus.RegRead32( this.base_address + GetAddrs(DMAC_TRANSFER_ID), transfer_id);
       `INFO(("Found transfer ID = %0d", transfer_id));
-    endtask
+    endtask : transfer_id_get
 
+    // -----------------
+    //
+    // -----------------
     task submit_transfer(dma_segment t,
                          output int next_transfer_id);
 
       dma_2d_segment t_2d;
-      dma_flocked_2d_segment t_fl_2d;
       int bus_max_width = `MAX(p.DMA_DATA_WIDTH_SRC, p.DMA_DATA_WIDTH_DEST)/8;
       int partial_reporting_en = 1;
-      int flock_en = 0;
 
       wait_transfer_submission();
       `INFO((" Submitting up a segment of : "));
@@ -186,56 +255,35 @@ package dmac_api_pkg;
         `ERROR(("Transfer length (%0d) must be multiple of largest interface (%0d)", t.length, bus_max_width));
       end
       if (p.DMA_TYPE_SRC == 0) begin
-        mng.RegWrite32( DMAC_BA + GetAddrs(dmac_SRC_ADDRESS),
-                                  SetField(dmac_SRC_ADDRESS, "SRC_ADDRESS", t.src_addr));
+        this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_SRC_ADDRESS),
+                                  SetField(DMAC_SRC_ADDRESS, "SRC_ADDRESS", t.src_addr));
       end
       if (p.DMA_TYPE_DEST == 0) begin
-        mng.RegWrite32( DMAC_BA + GetAddrs(dmac_DEST_ADDRESS),
-                                  SetField(dmac_DEST_ADDRESS, "DEST_ADDRESS", t.dst_addr));
+        this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_DEST_ADDRESS),
+                                  SetField(DMAC_DEST_ADDRESS, "DEST_ADDRESS", t.dst_addr));
       end
-      mng.RegWrite32( DMAC_BA + GetAddrs(dmac_X_LENGTH),
-                                SetField(dmac_X_LENGTH, "X_LENGTH", t.length-1));
+      this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_X_LENGTH),
+                                SetField(DMAC_X_LENGTH, "X_LENGTH", t.length-1));
 
       if (p.DMA_2D_TRANSFER == 1) begin
         if (!$cast(t_2d,t)) begin
-          // Write the default values for 2D regs for non-2D transcactions
-          t_2d = new();
+          // Write the default values for 2D regs for non-2D transactions
+          t_2d = new(p);
           t_2d.ylength = 1;
           t_2d.src_stride = 0;
           t_2d.dst_stride = 0;
         end
-        mng.RegWrite32( DMAC_BA + GetAddrs(dmac_Y_LENGTH),
-                                  SetField(dmac_Y_LENGTH, "Y_LENGTH", t_2d.ylength-1));
+        this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_Y_LENGTH),
+                                  SetField(DMAC_Y_LENGTH, "Y_LENGTH", t_2d.ylength-1));
         if (p.DMA_TYPE_SRC == 0) begin
-          mng.RegWrite32( DMAC_BA + GetAddrs(dmac_SRC_STRIDE),
-                                    SetField(dmac_SRC_STRIDE, "SRC_STRIDE", t_2d.src_stride));
+          this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_SRC_STRIDE),
+                                    SetField(DMAC_SRC_STRIDE, "SRC_STRIDE", t_2d.src_stride));
         end
         if (p.DMA_TYPE_DEST == 0) begin
-          mng.RegWrite32( DMAC_BA + GetAddrs(dmac_DEST_STRIDE),
-                                    SetField(dmac_DEST_STRIDE, "DEST_STRIDE", t_2d.dst_stride));
+          this.bus.RegWrite32( this.base_address + GetAddrs(DMAC_DEST_STRIDE),
+                                    SetField(DMAC_DEST_STRIDE, "DEST_STRIDE", t_2d.dst_stride));
         end
       end
-
-      /*if ($cast(t_fl_2d,t)) begin
-        flock_en = t_fl_2d.flock_en;
-        mng.RegWrite32( DMAC_BA + GetAddrs(dmac_FRAME_LOCK_CONFIG),
-                                  SetField(dmac_FRAME_LOCK_CONFIG, "FLOCK_NUMFRAMES", t_fl_2d.num_of_buffers) |
-                                  t_fl_2d.flock_mode << 8 |
-                                  t_fl_2d.flock_wait_master << 9 |
-                                  SetField(dmac_FRAME_LOCK_CONFIG, "FLOCK_FRAMEDISTANCE", t_fl_2d.frame_distance-1)
-                                  );
-        mng.RegWrite32( DMAC_BA + GetAddrs(dmac_FRAME_LOCK_STRIDE),
-                                  SetField(dmac_FRAME_LOCK_STRIDE, "FLOCK_FRAMESTRIDE", t_fl_2d.frame_stride)
-                                  );
-      end
-
-      mng.RegWrite32( DMAC_BA + GetAddrs(dmac_FLAGS),
-                                SetField(dmac_FLAGS, "FRAME_LOCK_EN", flock_en) |
-                                SetField(dmac_FLAGS, "PARTIAL_REPORTING_EN", partial_reporting_en) |
-                                SetField(dmac_FLAGS, "CYCLIC", t.cyclic) |
-                                SetField(dmac_FLAGS, "TLAST", t.last)
-                                );
-    */
 
       transfer_id_get(next_transfer_id);
       transfer_start();

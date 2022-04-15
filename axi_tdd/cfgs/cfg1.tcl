79bfbb24c68e266757d4f6189fc93a00cf65a98f
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2022-04-15 08:42:05 +0100

    axi_tdd: Add testbench for the Generic TDD engine

---
+++ axi_tdd/cfgs/cfg1.tcl
@@ -0,0 +1,13 @@
+global ad_project_params
+
+set tdd_cfg [list \
+  ID 0 \
+  CHANNEL_COUNT 8 \
+  REGISTER_WIDTH 32 \
+  BURST_COUNT_WIDTH 32 \
+  SYNC_EXTERNAL 1 \
+  SYNC_INTERNAL 1 \
+  SYNC_EXTERNAL_CDC 1 \
+  SYNC_COUNT_WIDTH 64 \
+  ]
+

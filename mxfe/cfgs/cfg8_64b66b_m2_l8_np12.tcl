98b1e416144b7d8226b144b7f6cf10964e14d271
Laszlo Nagy <laszlo.nagy@analog.com>
2022-08-19 16:16:42 +0100

    mxfe: Cfgs for m2 l8 np12

---
+++ mxfe/cfgs/cfg8_64b66b_m2_l8_np12.tcl
@@ -0,0 +1,22 @@
+global ad_project_params
+
+set ad_project_params(JESD_MODE) 64B66B
+set ad_project_params(RX_LANE_RATE) 12.375
+set ad_project_params(TX_LANE_RATE) 12.375
+set ad_project_params(REF_CLK_RATE) 250
+
+set ad_project_params(RX_NUM_LINKS) 1
+set ad_project_params(RX_JESD_M) 2
+set ad_project_params(RX_JESD_L) 8
+set ad_project_params(RX_JESD_S) 8
+set ad_project_params(RX_JESD_NP) 12
+set ad_project_params(RX_JESD_F) 3
+set ad_project_params(RX_JESD_K) 256
+
+set ad_project_params(TX_NUM_LINKS) 1
+set ad_project_params(TX_JESD_M) 2
+set ad_project_params(TX_JESD_L) 8
+set ad_project_params(TX_JESD_S) 8
+set ad_project_params(TX_JESD_NP) 12
+set ad_project_params(TX_JESD_F) 3
+set ad_project_params(TX_JESD_K) 256

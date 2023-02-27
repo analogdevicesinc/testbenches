757089a084defb27a30cc9883c7638dcc7c0274b
Sergiu Arpadi <sergiu.arpadi@analog.com>
2023-02-27 12:14:39 +0200

    tb_ad7606: Fix tb scripts

---
+++ ad7606/cfgs/cfg1.tcl
@@ -0,0 +1,13 @@
+global ad_project_params
+
+set ad_project_params(DEV_CONFIG) 0
+set ad_project_params(INTF) 1
+set ad_project_params(NUM_OF_SDI) 4
+set ad_project_params(SIMPLE_STATUS_CRC) 0
+set ad_project_params(EXT_CLK) 0
+
+#set DEV_CONFIG [get_env_param DEV_CONFIG 0]
+#set INTF [get_env_param INTF 0]
+#set NUM_OF_SDI [get_env_param NUM_OF_SDI 8]
+#set SIMPLE_STATUS_CRC [get_env_param SIMPLE_STATUS_CRC 0]
+#set EXT_CLK [get_env_param EXT_CLK 0]

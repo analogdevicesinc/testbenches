ccace945ab53df73131ed3365506c9318c107890
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2023-09-08 11:41:05 +0300

    dma_sg: Add testbench for the Scatter-Gather feature
    Signed-off-by: Ionut Podgoreanu <ionut.podgoreanu@analog.com>

---
+++ dma_sg/system_project.tcl
@@ -0,0 +1,45 @@
+source ../scripts/adi_sim.tcl
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+if {$argc < 1} {
+  puts "Expecting at least one argument that specifies the test configuration"
+  exit 1
+} else {
+  set cfg_file [lindex $argv 0]
+}
+
+# Read config file 
+source "cfgs/${cfg_file}"
+
+# Set the project name
+set project_name [file rootname $cfg_file]
+
+# Create the project
+adi_sim_project_xilinx $project_name "xcvu9p-flga2104-2L-e"
+
+# Add test files to the project
+adi_sim_project_files [list \
+ "../common/sv/utils.svh" \
+ "../common/sv/logger_pkg.sv" \
+ "../common/sv/reg_accessor.sv" \
+ "../common/sv/m_axis_sequencer.sv" \
+ "../common/sv/s_axis_sequencer.sv" \
+ "../common/sv/m_axi_sequencer.sv" \
+ "../common/sv/s_axi_sequencer.sv" \
+ "../common/sv/dmac_api.sv" \
+ "../common/sv/adi_regmap_pkg.sv" \
+ "../common/sv/adi_regmap_dmac_pkg.sv" \
+ "../common/sv/dma_trans.sv" \
+ "../common/sv/adi_peripheral_pkg.sv" \
+ "../common/sv/test_harness_env.sv" \
+ "tests/test_program_1d.sv" \
+ "tests/test_program_2d.sv" \
+ "tests/test_program_tr_queue.sv" \
+ "system_tb.sv" \
+ ]
+
+#set a default test program
+adi_sim_add_define "TEST_PROGRAM=test_program_1d"
+
+adi_sim_generate $project_name

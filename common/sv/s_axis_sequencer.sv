722cb376dfb2bf75bd1de5b4baa4382b1cd2c6f6
Istvan-Zsolt Szekely <istvan.szekely@analog.com>
2024-02-09 12:38:50 +0000

    s_axis_sequencer: Added base class on top for polymorphism
    Signed-off-by: Istvan-Zsolt Szekely <istvan.szekely@analog.com>

--- common/sv/s_axis_sequencer.sv
+++ common/sv/s_axis_sequencer.sv
@@ -40,20 +40,16 @@ package s_axis_sequencer_pkg;
   import axi4stream_vip_pkg::*;
   import logger_pkg::*;
 
-  class s_axis_sequencer #(type T);
+  class s_axis_sequencer_base;
 
-      protected T agent;
-      protected xil_axi4stream_data_byte byte_stream [$];
-      protected xil_axi4stream_ready_gen_policy_t mode;
-      protected xil_axi4stream_uint high_time;
-      protected xil_axi4stream_uint low_time;
+    protected xil_axi4stream_data_byte byte_stream [$];
+    protected xil_axi4stream_ready_gen_policy_t mode;
+    protected xil_axi4stream_uint high_time;
+    protected xil_axi4stream_uint low_time;
 
-    function new(T agent);
-      this.agent = agent;
-      this.mode = XIL_AXI4STREAM_READY_GEN_RANDOM;
-      this.low_time = 1;
-      this.high_time = 1;
-    endfunction
+    function new();
+      
+    endfunction: new
 
     function void set_mode(xil_axi4stream_ready_gen_policy_t mode);
       this.mode = mode;
@@ -79,37 +75,6 @@ package s_axis_sequencer_pkg;
       return this.low_time;
     endfunction
 
-    // TODO: test different ready policies
-
-    task user_gen_tready();
-      axi4stream_ready_gen tready_gen;
-      tready_gen = agent.driver.create_ready("TREADY");
-      tready_gen.set_ready_policy(this.mode);
-      if (this.mode != XIL_AXI4STREAM_READY_GEN_NO_BACKPRESSURE) begin
-        tready_gen.set_low_time(this.low_time);
-        tready_gen.set_high_time(this.high_time);
-      end
-      agent.driver.send_tready(tready_gen);
-    endtask
-
-    // Get transfer from the monitor and serialize data into a byte stream
-    // Assumption: all bytes from beat are valid (no position or null bytes)
-    task get_transfer();
-
-      axi4stream_monitor_transaction mytrans;
-      xil_axi4stream_data_beat  data_beat;
-
-      agent.monitor.item_collected_port.get(mytrans);
-
-      //$display(mytrans.convert2string);
-
-      data_beat = mytrans.get_data_beat();
-
-      for (int i=0; i<mytrans.get_data_width()/8; i++) begin
-        byte_stream.push_back(data_beat[i*8+:8]);
-      end
-    endtask;
-
     task verify_byte(bit [7:0] refdata);
       bit [7:0] data;
       if (byte_stream.size() == 0) begin
@@ -125,6 +90,57 @@ package s_axis_sequencer_pkg;
     task run();
       user_gen_tready();
     endtask
+
+    virtual task user_gen_tready();
+    endtask
+
+    virtual task get_transfer();
+    endtask
+
+  endclass: s_axis_sequencer_base
+
+
+  class s_axis_sequencer #( type T ) extends s_axis_sequencer_base;
+
+    protected T agent;
+
+    function new(T agent);
+      this.agent = agent;
+      this.mode = XIL_AXI4STREAM_READY_GEN_RANDOM;
+      this.low_time = 1;
+      this.high_time = 1;
+    endfunction
+
+    // TODO: test different ready policies
+
+    virtual task user_gen_tready();
+      axi4stream_ready_gen tready_gen;
+      tready_gen = agent.driver.create_ready("TREADY");
+      tready_gen.set_ready_policy(this.mode);
+      if (this.mode != XIL_AXI4STREAM_READY_GEN_NO_BACKPRESSURE) begin
+        tready_gen.set_low_time(this.low_time);
+        tready_gen.set_high_time(this.high_time);
+      end
+      agent.driver.send_tready(tready_gen);
+    endtask
+
+    // Get transfer from the monitor and serialize data into a byte stream
+    // Assumption: all bytes from beat are valid (no position or null bytes)
+    virtual task get_transfer();
+
+      axi4stream_monitor_transaction mytrans;
+      xil_axi4stream_data_beat  data_beat;
+
+      agent.monitor.item_collected_port.get(mytrans);
+
+      //$display(mytrans.convert2string);
+
+      data_beat = mytrans.get_data_beat();
+
+      for (int i=0; i<mytrans.get_data_width()/8; i++) begin
+        byte_stream.push_back(data_beat[i*8+:8]);
+      end
+    endtask;
 
   endclass
 

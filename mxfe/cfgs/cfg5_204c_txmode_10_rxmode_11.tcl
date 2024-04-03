7b071a44791d5e60ed5ff7d2e28534f68bf9d4ea
Stanca Pop <stanca.pop@analog.com>
2024-04-03 11:07:54 +0300

    mxfe: Use macros for IP BA
    Also, add TDD_SUPPORT param in all CFG files.

--- mxfe/cfgs/cfg5_204c_txmode_10_rxmode_11.tcl
+++ mxfe/cfgs/cfg5_204c_txmode_10_rxmode_11.tcl
@@ -22,3 +22,5 @@ set ad_project_params(TX_JESD_S) 1
 set ad_project_params(TX_JESD_NP) 16
 set ad_project_params(TX_JESD_F) 2
 set ad_project_params(TX_JESD_K) 128
+
+set ad_project_params(TDD_SUPPORT) 0

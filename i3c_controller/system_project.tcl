ce879f73a8f9b98960aed8d55047feb81a628c0a
Jorge Marques <2892061+gastmaier@users.noreply.github.com>
2024-04-12 09:21:57 -0300

    i3c_controller: Add testbench project (#66)
    Add a standalone project to test the implemented features in the I3C Controller framework
    (I3C Controller Host Interface and I3C Controller Core IP cores).
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

---
+++ i3c_controller/system_project.tcl
@@ -0,0 +1,74 @@
+# ***************************************************************************
+# ***************************************************************************
+# Copyright (C) 2024 Analog Devices, Inc. All rights reserved.
+#
+# In this HDL repository, there are many different and unique modules, consisting
+# of various HDL (Verilog or VHDL) components. The individual modules are
+# developed independently, and may be accompanied by separate and unique license
+# terms.
+#
+# The user should read each of these license terms, and understand the
+# freedoms and responsibilities that he or she has by using this source/core.
+#
+# This core is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.
+#
+# Redistribution and use of source or resulting binaries, with or without modification
+# of this file, are permitted under one of the following two license terms:
+#
+#   1. The GNU General Public License version 2 as published by the
+#      Free Software Foundation, which can be found in the top level directory
+#      of this repository (LICENSE_GPL2), and also online at:
+#      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+#
+# OR
+#
+#   2. An ADI specific BSD license, which can be found in the top level directory
+#      of this repository (LICENSE_ADIBSD), and also on-line at:
+#      https://github.com/analogdevicesinc/hdl/blob/main/LICENSE_ADIBSD
+#      This will allow to generate bit files and not release the source code,
+#      as long as it attaches to an ADI device.
+#
+# ***************************************************************************
+# ***************************************************************************
+
+source ../scripts/adi_sim.tcl
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+if {$argc < 1} {
+  puts "Expecting at least one argument that specifies the test configuration"
+  exit 1
+} else {
+  set cfg_file [lindex $argv 0]
+}
+
+# Read common config file
+source "cfgs/${cfg_file}"
+
+# Set the project name
+set project_name [file rootname $cfg_file]
+
+# Create the project
+adi_sim_project_xilinx $project_name "xcvu9p-flga2104-2L-e"
+
+# Add test files to the project
+adi_sim_project_files [list \
+ "../common/sv/utils.svh" \
+ "../common/sv/logger_pkg.sv" \
+ "../common/sv/reg_accessor.sv" \
+ "../common/sv/m_axis_sequencer.sv" \
+ "../common/sv/s_axis_sequencer.sv" \
+ "../common/sv/m_axi_sequencer.sv" \
+ "../common/sv/s_axi_sequencer.sv" \
+ "../common/sv/adi_regmap_pkg.sv" \
+ "../common/sv/test_harness_env.sv" \
+ "tests/test_program.sv" \
+ "system_tb.sv" \
+ ]
+
+#set a default test program
+adi_sim_add_define "TEST_PROGRAM=test_program"
+
+adi_sim_generate $project_name
---
+++ i3c_controller/system_tb.sv
@@ -0,0 +1,80 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright (C) 2024 Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsabilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/main/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/1ps
+
+`include "utils.svh"
+
+module system_tb();
+  wire i3c_irq;
+  wire i3c_clk;
+  wire i3c_scl;
+  wire i3c_sda;
+  wire i3c_sdi;
+  wire i3c_sdo;
+  wire i3c_t;
+  wire offload_sdi_valid;
+  wire offload_sdi_ready;
+  wire offload_trigger;
+
+  ad_iobuf #(
+    .DATA_WIDTH(1)
+  ) i_iobuf_sda (
+    .dio_t(i3c_t),
+    .dio_i(i3c_sdo),
+    .dio_o(i3c_sdi),
+    .dio_p(i3c_sda));
+
+  `TEST_PROGRAM test(
+    .i3c_irq(i3c_irq),
+    .i3c_clk(i3c_clk),
+    .i3c_scl(i3c_scl),
+    .i3c_sda(i3c_sda),
+    .offload_sdi_valid(offload_sdi_valid),
+    .offload_sdi_ready(offload_sdi_ready),
+    .offload_trigger(offload_trigger));
+
+  test_harness `TH (
+    .i3c_irq(i3c_irq),
+    .i3c_clk(i3c_clk),
+    .i3c_scl(i3c_scl),
+    .i3c_sdi(i3c_sdi),
+    .i3c_sdo(i3c_sdo),
+    .i3c_t(i3c_t),
+    .offload_sdi_tvalid(offload_sdi_valid),
+    .offload_sdi_tready(offload_sdi_ready),
+    .offload_trigger(offload_trigger));
+
+endmodule

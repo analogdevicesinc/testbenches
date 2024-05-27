35da4ecf0b30c868478f264906fe8d8e94b35345
IstvanZsSzekely <122256380+IstvanZsSzekely@users.noreply.github.com>
2024-05-27 09:59:26 +0300

    Base design (#64)
    Initial commit
    
    Signed-off-by: Istvan-Zsolt Szekely <istvan.szekely@analog.com>
    Co-authored-by: Jorge Marques <2892061+gastmaier@users.noreply.github.com>

---
+++ base/tests/test_program.sv
@@ -0,0 +1,86 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2024 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsabilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`include "utils.svh"
+
+import axi_vip_pkg::*;
+import axi4stream_vip_pkg::*;
+import logger_pkg::*;
+import environment_pkg::*;
+
+program test_program;
+
+  // Declare the class instances
+  environment env;
+
+  // Process variables
+  process current_process;
+  string current_process_random_state;
+  
+
+  initial begin
+
+    current_process = process::self();
+    current_process_random_state = current_process.get_randstate();
+    `INFO(("Randomization state: %s", current_process_random_state));
+
+    setLoggerVerbosity(250);
+
+    // Create environment
+    env = new(`TH.`SYS_CLK.inst.IF,
+              `TH.`DMA_CLK.inst.IF,
+              `TH.`DDR_CLK.inst.IF,
+              `TH.`SYS_RST.inst.IF,
+              `TH.`MNG_AXI.inst.IF,
+              `TH.`DDR_AXI.inst.IF
+             );
+
+    env.start();
+    env.sys_reset();
+
+    /* Add other configurations if necessary before calling run */
+
+    env.run();
+
+    /* Add stimulus tasks */
+        
+    env.stop();
+    
+    `INFO(("Test bench done!"));
+    $finish();
+
+  end
+
+endprogram

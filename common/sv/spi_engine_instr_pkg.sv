5fb71323d7d551e19857b6b2e112221282f02d1b
Laez Barbosa <laez.barbosa@analog.com>
2024-07-19 14:53:05 -0300

    SPI Engine: improve instruction macros
    Also get version from regmap
    
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

--- common/sv/spi_engine_instr_pkg.sv
+++ common/sv/spi_engine_instr_pkg.sv
@@ -1,10 +1,10 @@
 package spi_engine_instr_pkg;
 // Chip select instructions
-`define INST_CS         (32'h0000_1000)
-`define cs(mask)        (`INST_CS | (mask & 8'hFF))
-`define cs_delay(m,d)   (`INST_CS | ((d & 2'h3) << 8) | (m & 8'hFF))
-`define INST_CS_INV     (32'h0000_4000)
-`define cs_inv_mask(m)  (`INST_CS_INV | (m & 8'hFF))
+`define INST_CS             (32'h0000_1000)
+`define SET_CS(b)           (`INST_CS | (b & 8'hFF))
+`define SET_CS_DELAY(m,d)   (`INST_CS | ((d & 2'h3) << 8) | (m & 8'hFF))
+`define INST_CS_INV         (32'h0000_4000)
+`define SET_CS_INV_MASK(m)  (`INST_CS_INV | (m & 8'hFF))
 
 // Transfer instructions
 `define INST_WR         (32'h0000_0100 | (`NUM_OF_WORDS-1))
@@ -15,12 +15,13 @@ package spi_engine_instr_pkg;
 `define INST_CFG        (32'h0000_2100 | (`SDO_IDLE_STATE << 3) | (`THREE_WIRE << 2) | (`CPOL << 1) | `CPHA)
 `define INST_PRESCALE   (32'h0000_2000 | `CLOCK_DIVIDER)
 `define INST_DLENGTH    (32'h0000_2200 | `DATA_DLENGTH)
+`define SET_DLENGTH(d)  (`INST_DLENGTH | (d & 8'hFF))
 
 // Synchronization
 `define INST_SYNC       (32'h0000_3000)
 
 // Sleep instruction
 `define INST_SLEEP      (32'h0000_3100)
-`define sleep(a)        (`INST_SLEEP | (a & 8'hFF))
+`define SLEEP(a)        (`INST_SLEEP | (a & 8'hFF))
 
 endpackage

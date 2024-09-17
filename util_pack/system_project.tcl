14f476f284259b4adcd54bcb13c9497f60502d42
Istvan-Zsolt Szekely <istvan.szekely@analog.com>
2024-09-17 16:15:20 +0100

    irq_handler: Fixes after CI run
    Signed-off-by: Istvan-Zsolt Szekely <istvan.szekely@analog.com>

--- util_pack/system_project.tcl
+++ util_pack/system_project.tcl
@@ -29,6 +29,7 @@ adi_sim_project_files [list \
  "../common/sv/dmac_api.sv" \
  "../common/sv/dma_trans.sv" \
  "../common/sv/adi_regmap_dmac_pkg.sv" \
+ "environment.sv" \
  "tests/test_program.sv" \
  ]
 

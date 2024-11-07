68f80a1ce1177f261f6b5620947ce8673a253c70
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2024-11-07 17:00:43 +0200

    ad7405: Data acquisition test
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- testbenches/project/ad7405/tests/test_program.sv
+++ testbenches/project/ad7405/tests/test_program.sv
@@ -40,6 +40,7 @@
 import axi_vip_pkg::*;
 import axi4stream_vip_pkg::*;
 import adi_regmap_pkg::*;
+import adi_regmap_adc_pkg::*;
 import adi_regmap_common_pkg::*;
 import adi_regmap_clkgen_pkg::*;
 import adi_regmap_dmac_pkg::*;
@@ -50,11 +51,12 @@ localparam DATA_WIDTH                 = 16;
 localparam DATA_DLENGTH               = 16;
 localparam NUM_OF_WORDS               = 1;
 localparam NUM_OF_TRANSFERS           = 10;
+localparam AD7405_CTRL_RESETN         = 1;
 
 program test_program (
-  output       ad7405_adc_clk,
-  input        ad7405_adc_data,
-  input [15:0] ad7405_filter_dec_ratio);
+    input         ad7405_adc_clk,
+    output        ad7405_adc_data,
+    output [15:0] ad7405_filter_dec_ratio);
 
 
 test_harness_env env;
@@ -112,7 +114,7 @@ initial begin
 
   //#100
 
-  //data_acquisition_test();
+  data_acquisition_test();
 
   `INFO(("Test Done"));
 
@@ -128,6 +130,142 @@ task sanity_test();
   axi_write (`AXI_AD7405_BA + GetAddrs(COMMON_REG_SCRATCH), `SET_COMMON_REG_SCRATCH_SCRATCH(32'hDEADBEEF));
   axi_read_v (`AXI_AD7405_BA + GetAddrs(COMMON_REG_SCRATCH), `SET_COMMON_REG_SCRATCH_SCRATCH(32'hDEADBEEF));
   `INFO(("Sanity Test Done"));
+endtask
+
+//---------------------------------------------------------------------------
+// Data generator
+//---------------------------------------------------------------------------
+
+reg [15:0] sample = 0;
+reg [15:0] counter = 0;
+reg [15:0] sdi_shiftreg;
+reg        div_clk;
+reg        div_clk_s;
+
+assign ad7405_adc_data = sdi_shiftreg[15];
+
+initial begin
+  while(1) begin
+    @(posedge div_clk_s)
+    sample <= sample + 1'h1;
+  end
+end
+
+initial begin
+  while(1) begin
+    @(posedge ad7405_adc_clk);
+    counter <= counter + 1;
+  end
+end
+
+initial begin
+  while(1) begin
+    @(posedge ad7405_adc_clk);
+    if (div_clk == 1)
+        sdi_shiftreg <= sample;
+    else
+        sdi_shiftreg <= {sdi_shiftreg[14:0], 1'b0};
+    end
+end
+
+initial begin
+  while (1) begin
+    @(posedge ad7405_adc_clk);
+      div_clk_s <= counter[3];
+      div_clk <= (counter[3] && !div_clk_s);
+  end
+end
+
+//---------------------------------------------------------------------------
+// Storing Data for later comparison
+//---------------------------------------------------------------------------
+
+bit         offload_status = 0;
+bit         shiftreg_sampled = 0;
+bit [15:0]  sdi_store_cnt = 'h0;
+bit [31:0]  offload_sdi_data_store_arr [(NUM_OF_TRANSFERS) - 1:0];
+bit [DATA_DLENGTH-1:0]  sdi_data_store;
+
+initial begin
+  while (1) begin
+    @(posedge ad7405_adc_clk);
+    sdi_data_store <= {sdi_shiftreg[27:0], 4'b0};
+    if (sdi_data_store == 'h0 && shiftreg_sampled == 'h1 && sdi_shiftreg != 'h0) begin
+      shiftreg_sampled <= 'h0;
+      if (offload_status) begin
+        sdi_store_cnt <= sdi_store_cnt + 1;
+      end
+    end else if (shiftreg_sampled == 'h0 && sdi_data_store != 'h0) begin
+        if (offload_status) begin
+          offload_sdi_data_store_arr [sdi_store_cnt] = sdi_shiftreg;
+        end
+        shiftreg_sampled <= 'h1;
+      end
+  end
+end
+
+//---------------------------------------------------------------------------
+// Offload Transfer Counter
+//---------------------------------------------------------------------------
+
+bit [31:0] offload_transfer_cnt;
+
+initial begin
+  while(1) begin
+    @(posedge shiftreg_sampled && offload_status);
+      offload_transfer_cnt <= offload_transfer_cnt + 'h1;
+  end
+end
+
+//---------------------------------------------------------------------------
+// Data Acquisition Test
+//---------------------------------------------------------------------------
+
+bit [31:0] offload_captured_word_arr [(NUM_OF_TRANSFERS) -1 :0];
+reg [15:0] set_dec_rate = 16'h20;
+assign ad7405_filter_dec_ratio = set_dec_rate;
+
+task data_acquisition_test();
+
+    // Configure DMA
+    env.mng.RegWrite32(`AD7405_DMA_BA + GetAddrs(DMAC_CONTROL), `SET_DMAC_CONTROL_ENABLE(1)); // Enable DMA
+    env.mng.RegWrite32(`AD7405_DMA_BA + GetAddrs(DMAC_FLAGS),
+      `SET_DMAC_FLAGS_TLAST(1) |
+      `SET_DMAC_FLAGS_PARTIAL_REPORTING_EN(1)
+      ); // Use TLAST
+    env.mng.RegWrite32(`AD7405_DMA_BA + GetAddrs(DMAC_X_LENGTH), `SET_DMAC_X_LENGTH_X_LENGTH((NUM_OF_TRANSFERS*4)-1)); // X_LENGHTH
+    env.mng.RegWrite32(`AD7405_DMA_BA + GetAddrs(DMAC_DEST_ADDRESS), `SET_DMAC_DEST_ADDRESS_DEST_ADDRESS(`DDR_BA));  // DEST_ADDRESS
+    env.mng.RegWrite32(`AD7405_DMA_BA + GetAddrs(DMAC_TRANSFER_SUBMIT), `SET_DMAC_TRANSFER_SUBMIT_TRANSFER_SUBMIT(1)); // Submit transfer DMA
+
+    // Start axi clk generator
+    axi_write (`AD7405_AXI_CLKGEN_BA + GetAddrs(AXI_CLKGEN_REG_RSTN),
+      `SET_AXI_CLKGEN_REG_RSTN_MMCM_RSTN(1) |
+      `SET_AXI_CLKGEN_REG_RSTN_RSTN(1)
+      );
+
+    offload_status = 1;
+
+    // Configure AXI_AD7405
+    axi_write (`AXI_AD7405_BA + GetAddrs(ADC_COMMON_REG_RSTN), `SET_ADC_COMMON_REG_RSTN_RSTN(0));
+    #1000
+    axi_write (`AXI_AD7405_BA + GetAddrs(ADC_COMMON_REG_RSTN), `SET_ADC_COMMON_REG_RSTN_RSTN(AD7405_CTRL_RESETN));
+    axi_write (`AXI_AD7405_BA + GetAddrs(ADC_CHANNEL_REG_CHAN_CNTRL), `SET_ADC_CHANNEL_REG_CHAN_CNTRL_ENABLE(1));
+
+    wait(offload_transfer_cnt == NUM_OF_TRANSFERS);
+
+    offload_status = 0;
+
+    for (int i=0; i<=((NUM_OF_TRANSFERS) -1); i=i+1) begin
+      #1
+      offload_captured_word_arr[i] = env.ddr_axi_agent.mem_model.backdoor_memory_read_4byte(`DDR_BA + 4*i);
+    end
+
+    if (offload_captured_word_arr [(NUM_OF_TRANSFERS) - 1:2] != offload_sdi_data_store_arr [(NUM_OF_TRANSFERS) - 1:2]) begin
+      `ERROR(("Offload Test FAILED"));
+    end else begin
+      `INFO(("Offload Test PASSED"));
+    end
+
 endtask
 
 endprogram

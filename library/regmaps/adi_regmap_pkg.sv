8120332a5d24651f9378c494b2ea8934eecdc73a
Istvan-Zsolt Szekely <istvan.szekely@analog.com>
2024-11-08 09:10:06 +0200

    regmap: Minor changes and updates
    Signed-off-by: Istvan-Zsolt Szekely <istvan.szekely@analog.com>

--- library/regmaps/adi_regmap_pkg.sv
+++ library/regmaps/adi_regmap_pkg.sv
@@ -35,120 +35,15 @@
 
 `include "utils.svh"
 
-package adi_regmap_pkg;
-
-  import logger_pkg::*;
-
-  typedef enum {NA, R, RO, ROV, RW, RW1C, RW1CV, RW1S, W1S, WO} acc_t;
-
-  // Used in generic definitions
-  const int n = 0;
-
-  typedef struct {
-    int msb;
-    int lsb;
-    acc_t access;
-    int reset_value;
-  } field_t;
-
-  typedef struct{
-    int addr;
-    string name;
-    field_t fields[string];
-  } reg_t;
-
-
-  function bit [31:0] SetField(reg_t register,
-                               string field,
-                               bit [31:0] value);
-    bit [31:0] ret = 'h0;
-    int lsb, msb;
-
-    if (!register.fields.exists(field))
-      `ERROR(("Field %s in reg %s does not exists", field, register.name));
-
-    lsb = register.fields[field].lsb;
-    msb = register.fields[field].msb;
-
-    ret = value << lsb;
-    for (int i=msb+1;i<=31;i++) begin
-      ret[i]=1'b0;
-    end
-
-    `INFOV(("Setting reg %s[%0d:%0d] field %s with %h (%h)", register.name, msb, lsb, field, value, ret), 10);
-
-    return ret;
-  endfunction;
-
-  function bit [31:0] GetField(reg_t register,
-                               string field,
-                               bit [31:0] regvalue);
-    bit [31:0] ret = 'h0;
-    int lsb, msb;
-
-    if (!register.fields.exists(field))
-      `ERROR(("Field %s in reg %s does not exists", field, register.name));
-
-    lsb = register.fields[field].lsb;
-    msb = register.fields[field].msb;
-
-    for (int i=msb+1;i<=31;i++) begin
-      regvalue[i]=1'b0;
-    end
-    ret = regvalue >> lsb;
-
-//    `INFOV(("Setting reg %s[%0d:%0d] field %s with %h (%h)", register.name, msb, lsb, field, value, ret), 4);
-
-    return ret;
-  endfunction;
-
-  function bit [31:0] UpdateField(reg_t register,
-                                  string field,
-                                  bit [31:0] regvalue,
-                                  bit [31:0] curregvalue);
-    bit [31:0] ret = curregvalue;
-
-    if (!register.fields.exists(field))
-      `ERROR(("Field %s in reg %s does not exists", field, register.name));
-
-    ret = ret & (~SetField(register, field, 'hFFFF)); // mask
-    ret = ret | SetField(register, field, regvalue); // update register
-
-    return ret;
-  endfunction
-
-  function int GetAddrs(reg_t register);
-    return register.addr;
-  endfunction;
-
-  function int GetResetValue(reg_t register, 
-                             string field);
-    return register.fields[field].reset_value;
-  endfunction;
-
-endpackage
-
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-
 package regmap_pkg;
+
   import logger_pkg::*;
 
   typedef enum {NA, R, RO, ROV, RW, RW1C, RW1CV, RW1S, W1S, WO} acc_t;
-
-  typedef struct {
-    int msb;
-    int lsb;
-    acc_t access;
-    logic [31:0] reset_value;
-  } field_t;
 
 
   class register_base;
+
     protected string name;
     logic [31:0] value;
     protected logic [31:0] reset_value;
@@ -190,7 +85,7 @@ package regmap_pkg;
 
       `INFOV(("Setting reg %s with reset value %h (%h)", this.name, reset_value, this.reset_value), 10);
     
-      this.reset_value = reset_value;
+      this.reset_value = this.reset_value | reset_value;
     endfunction
 
     function int get_address();
@@ -200,9 +95,12 @@ package regmap_pkg;
     function string get_name();
       return this.name;
     endfunction
+
   endclass
 
+
   class field_base;
+
     local string name;
     local int msb;
     local int lsb;
@@ -233,14 +131,14 @@ package regmap_pkg;
         update_value[i]=1'b0;
       end
 
-      this.reg_handle.set_reset_value(this.reg_handle.get_reset_value() | update_value);
+      this.reg_handle.set_reset_value(update_value);
     endfunction
 
     function logic [31:0] get();
       automatic logic [31:0] value = 'h0;
       automatic logic [31:0] regvalue = this.reg_handle.get();
 
-      for (int i=this.msb+1;i<=31;i++) begin
+      for (int i=this.msb+1; i<32; i++) begin
         regvalue[i]=1'b0;
       end
       value = regvalue >> this.lsb;
@@ -254,13 +152,16 @@ package regmap_pkg;
       automatic logic [31:0] update_value = 'h0;
       automatic logic [31:0] mask = 'hFFFF;
 
+      if (this.access == acc_t::NA || this.access == acc_t::R || this.access == acc_t::RO || this.access == acc_t::ROV)
+        `ERROR(("Modifying a read only field!"));
+
       update_value = set_value << this.lsb;
       for (int i=this.msb+1;i<=31;i++) begin
         update_value[i]=1'b0;
       end
 
       mask = mask << this.lsb;
-      for (int i=this.msb+1;i<=31;i++) begin
+      for (int i=this.msb+1; i<32; i++) begin
         mask[i]=1'b0;
       end
 
@@ -275,5 +176,7 @@ package regmap_pkg;
 
       return this.reset_value;
     endfunction
+
   endclass
+  
 endpackage

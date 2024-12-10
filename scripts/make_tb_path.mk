a9797fe1b5a39d6cbc7295bdea2558daa3d8b3b4
Jorge Marques <jorge.marques@analog.com>
2024-12-10 10:23:44 -0300

    scripts: make_tb_path: Error for unset ADI_*_DIR
    Add informative error for mandatory environment variable.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- scripts/make_tb_path.mk
+++ scripts/make_tb_path.mk
@@ -2,8 +2,14 @@
 ####################################################################################
 ####################################################################################
 
+ifeq ($(ADI_HDL_DIR),)
+$(error Environment variable ADI_HDL_DIR not set, please set it with the absolute path to the hdl repository.)
+endif
+
+ifeq ($(ADI_TB_DIR),)
+$(error Environment variable ADI_TB_DIR not set, please set it with the absolute path to the testbenches repository.)
+endif
+
 # Assumes this file is in <HDL>/testbenches/scripts/make_tb_path.mk
-ADI_HDL_DIR := ${ADI_HDL_DIR}
 HDL_LIBRARY_PATH := $(ADI_HDL_DIR)/library/
-ADI_TB_DIR := ${ADI_TB_DIR}
 TB_LIBRARY_PATH := $(ADI_TB_DIR)/library/

8446686b33b1d1f51daaf7ebfa4b6fa23da98aa1
Laez Barbosa <laez.barbosa@analog.com>
2025-01-30 10:00:09 -0300

    spi_engine: update for hdl 1.4.0
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

--- testbenches/ip/spi_engine/cfgs/cfg11.tcl
+++ testbenches/ip/spi_engine/cfgs/cfg11.tcl
@@ -14,12 +14,12 @@ set ad_project_params(SDI_FIFO_ADDR_WIDTH)  5
 set ad_project_params(SDO_FIFO_ADDR_WIDTH)  5
 set ad_project_params(SYNC_FIFO_ADDR_WIDTH) 4
 set ad_project_params(CMD_FIFO_ADDR_WIDTH)  4
-set ad_project_params(SDO_STREAMING)        1
+set ad_project_params(SDO_STREAMING)        0
 
 # Test parameters
 set ad_project_params(DATA_DLENGTH)         18
 set ad_project_params(THREE_WIRE)           0
-set ad_project_params(CPOL)                 0
+set ad_project_params(CPOL)                 1
 set ad_project_params(CPHA)                 1
 set ad_project_params(SDO_IDLE_STATE)       0
 set ad_project_params(SLAVE_TIN)            0
@@ -28,8 +28,8 @@ set ad_project_params(MASTER_TIN)           0
 set ad_project_params(MASTER_TOUT)          0
 set ad_project_params(CS_TO_MISO)           0
 set ad_project_params(CLOCK_DIVIDER)        2
-set ad_project_params(NUM_OF_WORDS)         5
-set ad_project_params(NUM_OF_TRANSFERS)     3
+set ad_project_params(NUM_OF_WORDS)         3
+set ad_project_params(NUM_OF_TRANSFERS)     5
 set ad_project_params(CS_ACTIVE_HIGH)       0
 set ad_project_params(ECHO_SCLK_DELAY)      0.1
 

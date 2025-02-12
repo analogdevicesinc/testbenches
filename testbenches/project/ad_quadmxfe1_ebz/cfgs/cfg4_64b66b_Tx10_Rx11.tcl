76200e59b48ed47e42e78bdac6c914a2d0c24cec
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2025-02-12 11:20:33 +0200

    ad_quadmxfe1_ebz: Add Data Offload support
    Signed-off-by: Ionut Podgoreanu <ionut.podgoreanu@analog.com>

--- testbenches/project/ad_quadmxfe1_ebz/cfgs/cfg4_64b66b_Tx10_Rx11.tcl
+++ testbenches/project/ad_quadmxfe1_ebz/cfgs/cfg4_64b66b_Tx10_Rx11.tcl
@@ -1,5 +1,11 @@
-
 global ad_project_params
+
+set ad_project_params(ADC_OFFLOAD_TYPE) 0
+set ad_project_params(ADC_OFFLOAD_SIZE) [expr 2*1024*1024]
+set ad_project_params(DAC_OFFLOAD_TYPE) 0
+set ad_project_params(DAC_OFFLOAD_SIZE) [expr 2*1024*1024]
+set ad_project_params(RD_DATA_REGISTERED) 1
+set ad_project_params(RD_FIFO_ADDRESS_WIDTH) 3
 
 set ad_project_params(JESD_MODE)  64B66B
 set ad_project_params(RX_LANE_RATE)  8.25
@@ -7,6 +13,7 @@ set ad_project_params(TX_LANE_RATE)  8.25
 set ad_project_params(RX_PLL_SEL)  2
 set ad_project_params(TX_PLL_SEL)  2
 set ad_project_params(REF_CLK_RATE)  250
+
 set ad_project_params(RX_JESD_M)  4
 set ad_project_params(RX_JESD_L)  4
 set ad_project_params(RX_JESD_F)  2
@@ -14,6 +21,7 @@ set ad_project_params(RX_JESD_S)  1
 set ad_project_params(RX_JESD_NP)  16
 set ad_project_params(RX_JESD_K)  128
 set ad_project_params(RX_NUM_LINKS)  4
+
 set ad_project_params(TX_JESD_M)  4
 set ad_project_params(TX_JESD_L)  4
 set ad_project_params(TX_JESD_F)  2
@@ -21,5 +29,6 @@ set ad_project_params(TX_JESD_S)  1
 set ad_project_params(TX_JESD_NP)  16
 set ad_project_params(TX_JESD_K)  128
 set ad_project_params(TX_NUM_LINKS)  4
+
 set ad_project_params(DAC_TPL_XBAR_ENABLE)  0
 

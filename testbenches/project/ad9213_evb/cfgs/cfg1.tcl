4dd16251a89c059a5864c77bd9a3ce88fced7786
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2025-03-04 16:40:47 +0200

    ad9213_evb: Add testbench
    Signed-off-by: Ionut Podgoreanu <ionut.podgoreanu@analog.com>

---
+++ testbenches/project/ad9213_evb/cfgs/cfg1.tcl
@@ -0,0 +1,14 @@
+global ad_project_params
+
+set ad_project_params(REF_CLK_RATE) 500
+set ad_project_params(LANE_RATE) 10
+
+set ad_project_params(ADC_OFFLOAD_TYPE) 0
+set ad_project_params(ADC_OFFLOAD_SIZE) [expr 512*1024]
+
+set ad_project_params(RX_JESD_M) 1
+set ad_project_params(RX_JESD_L) 16
+set ad_project_params(RX_JESD_S) 16
+set ad_project_params(RX_JESD_NP) 16
+set ad_project_params(RX_JESD_F) 2
+set ad_project_params(RX_JESD_K) 32

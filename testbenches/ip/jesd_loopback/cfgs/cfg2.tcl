7c0a374cce756be5fd080cb4082dc59b98a17376
Stanca Pop <stanca.pop@analog.com>
2025-04-10 12:21:24 +0300

    testbenches/ip/jesd_loopback: Rearrange cfg JESD parameters
    Signed-off-by: Stanca Pop <stanca.pop@analog.com>

--- testbenches/ip/jesd_loopback/cfgs/cfg2.tcl
+++ testbenches/ip/jesd_loopback/cfgs/cfg2.tcl
@@ -5,8 +5,8 @@ set ad_project_params(LINK_MODE) $JESD_8B10B
 set ad_project_params(JESD_M) 16
 set ad_project_params(JESD_L) 8
 set ad_project_params(JESD_F) 4
-set ad_project_params(JESD_S) 1
 set ad_project_params(JESD_K) 32
+set ad_project_params(JESD_S) 1
 set ad_project_params(JESD_NP) 16
 
 set ad_project_params(REF_CLK_RATE) 250
--- testbenches/ip/jesd_loopback/cfgs/cfg2_np12.tcl
+++ testbenches/ip/jesd_loopback/cfgs/cfg2_np12.tcl
@@ -2,8 +2,8 @@ source [file dirname [file normalize [info script]]]/common.tcl
 
 set ad_project_params(LINK_MODE) $JESD_8B10B
 
-set ad_project_params(JESD_L) 2
 set ad_project_params(JESD_M) 4
+set ad_project_params(JESD_L) 2
 set ad_project_params(JESD_F) 3
 set ad_project_params(JESD_K) 32
 set ad_project_params(JESD_S) 1

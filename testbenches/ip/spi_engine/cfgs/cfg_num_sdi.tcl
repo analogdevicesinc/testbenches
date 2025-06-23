0da675279c36740ed383260f90eff4bad35da451
caosjr <carlos.souza@analog.com>
2025-06-23 10:46:24 -0300

    testbenches/ip/spi_engine: update to support n lanes
    Several changes to support more than one lane for SDI and
    SDO:
    * Update config files;
    * Insert a new test for lane mask.
    
    Signed-off-by: Carlos Souza <carlos.souza@analog.com>

--- testbenches/ip/spi_engine/cfgs/cfg_num_sdi.tcl
+++ testbenches/ip/spi_engine/cfgs/cfg_num_sdi.tcl
@@ -4,22 +4,22 @@ global ad_project_params
 set ad_project_params(DATA_WIDTH)           32
 set ad_project_params(ASYNC_SPI_CLK)        1
 set ad_project_params(NUM_OF_CS)            1
-set ad_project_params(NUM_OF_SDI)           1
-set ad_project_params(NUM_OF_SDO)           1
-set ad_project_params(SDI_DELAY)            1
+set ad_project_params(NUM_OF_SDI)           4
+set ad_project_params(NUM_OF_SDO)           4
+set ad_project_params(SDI_DELAY)            0
 set ad_project_params(ECHO_SCLK)            0
 set ad_project_params(CMD_MEM_ADDR_WIDTH)   4
 set ad_project_params(DATA_MEM_ADDR_WIDTH)  4
-set ad_project_params(SDI_FIFO_ADDR_WIDTH)  5
-set ad_project_params(SDO_FIFO_ADDR_WIDTH)  5
+set ad_project_params(SDI_FIFO_ADDR_WIDTH)  7
+set ad_project_params(SDO_FIFO_ADDR_WIDTH)  7
 set ad_project_params(SYNC_FIFO_ADDR_WIDTH) 4
 set ad_project_params(CMD_FIFO_ADDR_WIDTH)  4
-set ad_project_params(SDO_STREAMING)        1
+set ad_project_params(SDO_STREAMING)        0
 
-# Test parameters
-set ad_project_params(DATA_DLENGTH)         18
+# Test parameterscfg_nu
+set ad_project_params(DATA_DLENGTH)         8
 set ad_project_params(THREE_WIRE)           0
-set ad_project_params(CPOL)                 0
+set ad_project_params(CPOL)                 1
 set ad_project_params(CPHA)                 1
 set ad_project_params(SDO_IDLE_STATE)       0
 set ad_project_params(SLAVE_TIN)            0
@@ -28,11 +28,12 @@ set ad_project_params(MASTER_TIN)           0
 set ad_project_params(MASTER_TOUT)          0
 set ad_project_params(CS_TO_MISO)           0
 set ad_project_params(CLOCK_DIVIDER)        2
-set ad_project_params(NUM_OF_WORDS)         5
-set ad_project_params(NUM_OF_TRANSFERS)     3
-set ad_project_params(SPI_LANE_MASK)        1
+set ad_project_params(NUM_OF_WORDS)         3
+set ad_project_params(NUM_OF_TRANSFERS)     5
+set ad_project_params(SPI_LANE_MASK)        15
 set ad_project_params(CS_ACTIVE_HIGH)       0
 set ad_project_params(ECHO_SCLK_DELAY)      0.1
+
 
 set spi_s_vip_cfg [ list \
     MODE             0                                    \

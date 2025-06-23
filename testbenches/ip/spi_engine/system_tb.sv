097617e114a51dec1da614b2e1b23a0aa40000cf
caosjr <carlos.souza@analog.com>
2025-06-23 10:46:24 -0300

    testbenches/ip/spi_engine: update to support n lanes
    Several changes to support more than one lane for SDI and
    SDO:
    * Update config files. All config files have 4 lanes now;
    * Insert a new test for lane mask.
    
    Signed-off-by: Carlos Souza <carlos.souza@analog.com>

--- testbenches/ip/spi_engine/system_tb.sv
+++ testbenches/ip/spi_engine/system_tb.sv
@@ -36,15 +36,15 @@
 `include "utils.svh"
 
 module system_tb();
-  wire spi_engine_spi_cs;
-  wire spi_engine_spi_sclk;
-  wire spi_engine_spi_clk;
-  wire spi_engine_spi_sdi;
-  wire spi_engine_spi_sdo;
-  wire spi_engine_irq;
-`ifdef DEF_ECHO_SCLK
-  wire spi_engine_echo_sclk;
-`endif
+  wire                     spi_engine_spi_sclk;
+  wire                     spi_engine_spi_clk;
+  wire [(`NUM_OF_CS-1) :0] spi_engine_spi_cs;
+  wire [(`NUM_OF_SDI-1):0] spi_engine_spi_sdi;
+  wire [(`NUM_OF_SDO-1):0] spi_engine_spi_sdo;
+  wire                     spi_engine_irq;
+  `ifdef DEF_ECHO_SCLK
+  wire                     spi_engine_echo_sclk;
+  `endif
 
   `TEST_PROGRAM test(
     .spi_engine_irq(spi_engine_irq),

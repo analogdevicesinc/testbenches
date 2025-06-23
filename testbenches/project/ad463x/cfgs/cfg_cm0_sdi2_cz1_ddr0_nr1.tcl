0f8cc0a12c26075e2fa4d44fa05a54c1a9680876
Liviu 'Ceshu' Adace <liviu.adace@analog.com>
2025-06-23 11:55:38 +0300

    project: ad463x: New configuration with spi_axi_reorder IP removed
    Signed-off-by: Liviu 'Ceshu' Adace <liviu.adace@analog.com>

---
+++ testbenches/project/ad463x/cfgs/cfg_cm0_sdi2_cz1_ddr0_nr1.tcl
@@ -0,0 +1,8 @@
+global ad_project_params
+
+set ad_project_params(CLK_MODE) 0
+set ad_project_params(NUM_OF_SDI) 2
+set ad_project_params(CAPTURE_ZONE) 1
+set ad_project_params(DDR_EN) 0
+set ad_project_params(NO_REORDER) 1
+
--- testbenches/project/ad463x/cfgs/cfg_cm0_sdi4_cz2_ddr0_nr0.tcl
+++ testbenches/project/ad463x/cfgs/cfg_cm0_sdi4_cz2_ddr0_nr0.tcl
@@ -1,7 +1,8 @@
 global ad_project_params
 
 set ad_project_params(CLK_MODE) 0
-set ad_project_params(NUM_OF_SDI) 8
+set ad_project_params(NUM_OF_SDI) 4
 set ad_project_params(CAPTURE_ZONE) 2
 set ad_project_params(DDR_EN) 0
+set ad_project_params(NO_REORDER) 0
 

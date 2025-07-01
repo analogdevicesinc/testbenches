292b4ba18bda516f7a14a7568fdb84c54cd06e50
Istvan-Zsolt Szekely <istvan.szekely@analog.com>
2025-07-01 14:28:09 +0300

    adi_datatypes: Added new functions to the FIFO and LIFO structures
    - Added functions include: Clone, Copy and Compare
    
    Signed-off-by: Istvan-Zsolt Szekely <istvan.szekely@analog.com>

--- library/utilities/adi_datatypes.sv
+++ library/utilities/adi_datatypes.sv
@@ -42,7 +42,7 @@ package adi_datatypes_pkg;
 
   class adi_fifo #(type data_type = int) extends adi_component;
 
-    local data_type adi_fifo [$];
+    local data_type fifo [$];
     local int depth;
 
     function new(
@@ -55,53 +55,119 @@ package adi_datatypes_pkg;
       this.depth = depth;
     endfunction: new
 
+    function adi_component clone();
+      adi_fifo #(data_type) object;
+      adi_component parent;
+
+      $cast(parent, this.parent);
+
+      object = new(
+        .name(this.name),
+        .depth(this.depth),
+        .parent(parent));
+
+      this.copy(object);
+
+      return object;
+    endfunction: clone
+
+    function void copy(input adi_component object);
+      adi_fifo #(data_type) cast_object;
+      data_type temporary_storage;
+      int depth;
+
+      $cast(cast_object, object);
+
+      depth = this.size();
+
+      for (int i=0; i<depth; i++) begin
+        temporary_storage = this.pop();
+        void'(this.push(temporary_storage));
+
+        void'(cast_object.push(temporary_storage));
+      end
+    endfunction: copy
+
+    function bit compare(
+      input adi_component object,
+      input bit logical_equality = 1'b0);
+
+      adi_fifo #(data_type) cast_object;
+      data_type local_storage;
+      data_type object_storage;
+      int depth;
+      bit result = 1;
+
+      $cast(cast_object, object);
+
+      depth = this.size();
+
+      if (depth != cast_object.size()) begin
+        return 0;
+      end
+      for (int i=0; i<depth; i++) begin
+        local_storage = this.pop();
+        void'(this.push(local_storage));
+
+        object_storage = cast_object.pop();
+        void'(cast_object.push(object_storage));
+
+        if (logical_equality == 1'b1) begin
+          if (local_storage != object_storage) begin
+            result = 0;
+          end
+        end else begin
+          if (local_storage !== object_storage) begin
+            result = 0;
+          end
+        end
+      end
+      return result;
+    endfunction: compare
+
     function bit push(input data_type data);
-      if (this.adi_fifo.size() == this.depth && this.depth != 0) begin
+      if (this.fifo.size() == this.depth && this.depth != 0) begin
         return 1'b0;
       end else begin
-        this.adi_fifo.push_back(data);
+        this.fifo.push_back(data);
         return 1'b1;
       end
     endfunction: push
 
     function data_type pop();
-      if (this.adi_fifo.size() == 0) begin
-        return null;
-      end else begin
-        return this.adi_fifo.pop_front();
-      end
+      return this.fifo.pop_front();
     endfunction: pop
 
     function int room();
-      return depth-this.adi_fifo.size();
+      return this.depth-this.fifo.size();
     endfunction: room
 
     function int size();
-      return this.adi_fifo.size();
-    endfunction: room
+      return this.fifo.size();
+    endfunction: size
 
     function void clear();
-      this.adi_fifo.delete();
+      this.fifo.delete();
     endfunction: clear
 
     function bit insert(
       input int index,
       input data_type data);
 
-      if (this.adi_fifo.size() == this.depth && this.depth != 0) begin
+      if (this.fifo.size() == this.depth && this.depth != 0) begin
         return 1'b0;
       end else begin
-        this.adi_fifo.insert(index, data);
+        this.fifo.insert(index, data);
         return 1'b1;
       end
-    endfunction: clear
+    endfunction: insert
 
   endclass: adi_fifo
 
 
   class adi_lifo #(type data_type = int) extends adi_component;
 
-    local data_type adi_fifo [$];
+    local data_type lifo [$];
     local int depth;
 
     function new(
@@ -114,46 +180,141 @@ package adi_datatypes_pkg;
       this.depth = depth;
     endfunction: new
 
+    function adi_component clone();
+      adi_lifo #(data_type) object;
+      adi_component parent;
+
+      $cast(parent, this.parent);
+
+      object = new(
+        .name(this.name),
+        .depth(this.depth),
+        .parent(parent));
+
+      this.copy(object);
+
+      return object;
+    endfunction: clone
+
+    function void copy(input adi_component object);
+      adi_lifo #(data_type) cast_object;
+      adi_lifo #(data_type) temporary_lifo;
+      data_type temporary_storage;
+      int depth;
+
+      $cast(cast_object, object);
+
+      temporary_lifo = new(
+        .name("Temp storage"),
+        .depth(this.depth));
+
+      depth = this.size();
+
+      for (int i=0; i<depth; i++) begin
+        void'(temporary_lifo.push(this.pop()));
+      end
+
+      for (int i=0; i<depth; i++) begin
+        temporary_storage = temporary_lifo.pop();
+
+        void'(this.push(temporary_storage));
+
+        void'(cast_object.push(temporary_storage));
+      end
+    endfunction: copy
+
+    function bit compare(
+      input adi_component object,
+      input bit logical_equality = 1'b0);
+
+      adi_lifo #(data_type) cast_object;
+      adi_lifo #(data_type) temporary_local_lifo;
+      adi_lifo #(data_type) temporary_object_lifo;
+      data_type local_storage;
+      data_type object_storage;
+      int depth;
+      bit result = 1;
+
+      $cast(cast_object, object);
+
+      temporary_local_lifo = new(
+        .name("Local LIFO"),
+        .depth(this.depth));
+
+      temporary_object_lifo = new(
+        .name("Object LIFO"),
+        .depth(this.depth));
+
+      depth = this.size();
+
+      if (depth != cast_object.size()) begin
+        return 0;
+      end
+
+      for (int i=0; i<depth; i++) begin
+        local_storage = this.pop();
+        void'(temporary_local_lifo.push(local_storage));
+
+        object_storage = cast_object.pop();
+        void'(temporary_object_lifo.push(object_storage));
+      end
+
+      for (int i=0; i<depth; i++) begin
+        local_storage = temporary_local_lifo.pop();
+        void'(this.push(local_storage));
+
+        object_storage = temporary_object_lifo.pop();
+        void'(cast_object.push(object_storage));
+
+        if (logical_equality == 1'b1) begin
+          if (local_storage != object_storage) begin
+            result = 0;
+          end
+        end else begin
+          if (local_storage !== object_storage) begin
+            result = 0;
+          end
+        end
+      end
+      return result;
+    endfunction: compare
+
     function bit push(input data_type data);
-      if (this.adi_fifo.size() == this.depth && this.depth != 0) begin
+      if (this.lifo.size() == this.depth && this.depth != 0) begin
         return 1'b0;
       end else begin
-        this.adi_fifo.push_front(data);
+        this.lifo.push_front(data);
         return 1'b1;
       end
     endfunction: push
 
     function data_type pop();
-      if (this.adi_fifo.size() == 0) begin
-        return null;
-      end else begin
-        return this.adi_fifo.pop_front();
-      end
+      return this.lifo.pop_front();
     endfunction: pop
 
     function int room();
-      return depth-this.adi_fifo.size();
+      return depth-this.lifo.size();
     endfunction: room
 
     function int size();
-      return this.adi_fifo.size();
-    endfunction: room
+      return this.lifo.size();
+    endfunction: size
 
     function void clear();
-      this.adi_fifo.delete();
+      this.lifo.delete();
     endfunction: clear
 
     function bit insert(
       input int index,
       input data_type data);
 
-      if (this.adi_fifo.size() == this.depth && this.depth != 0) begin
+      if (this.lifo.size() == this.depth && this.depth != 0) begin
         return 1'b0;
       end else begin
-        this.adi_fifo.insert(index, data);
+        this.lifo.insert(index, data);
         return 1'b1;
       end
-    endfunction: clear
+    endfunction: insert
 
   endclass: adi_lifo
 

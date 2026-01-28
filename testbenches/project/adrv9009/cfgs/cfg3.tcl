5568988d52f0b63004cceacf09cc3fa3fdd3b814
Elena-Hadarau_adi <Elena.Hadarau@analog.com>
2026-01-28 17:23:27 +0200

    testbenches/project: Add board randomization
    Signed-off-by: Elena-Hadarau_adi <Elena.Hadarau@analog.com>

--- testbenches/project/adrv9009/cfgs/cfg3.tcl
+++ testbenches/project/adrv9009/cfgs/cfg3.tcl
@@ -4,6 +4,7 @@ set ad_project_params(LINK_MODE) 1
 
 set ad_project_params(REF_CLK_RATE) 500
 set ad_project_params(LANE_RATE) 10
+set ad_project_params(PLL_TYPE) QPLL0
 
 set ad_project_params(DAC_OFFLOAD_TYPE) 0
 set ad_project_params(DAC_OFFLOAD_SIZE) [expr 2*1024*1024]
@@ -29,3 +30,7 @@ set ad_project_params(RX_OS_JESD_S) 1
 set ad_project_params(RX_OS_JESD_NP) 16
 set ad_project_params(RX_OS_JESD_F) 4
 set ad_project_params(RX_OS_JESD_K) 32
+
+set xilinx_boards {"zc706" "zcu102" "kcu105"}
+set chosen_board [lindex $xilinx_boards [expr {int(rand() * [llength $xilinx_boards])}]]
+set ad_project_params(FPGA_BOARD) $chosen_board

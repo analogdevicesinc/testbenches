a5a591a5b12a308a630421f6bda439259c3cdc8a
Elena-Hadarau_adi <Elena.Hadarau@analog.com>
2026-01-28 17:05:16 +0200

    scripts/adi_sim: Add support for xcvr automation and board name
    Signed-off-by: Elena-Hadarau_adi <Elena.Hadarau@analog.com>

--- testbenches/project/fmcomms2/cfgs/cfg1.tcl
+++ testbenches/project/fmcomms2/cfgs/cfg1.tcl
@@ -1,7 +1,7 @@
 global ad_project_params
 
-
 # Put project configs here
 # e.g.
 # set ad_project_params(CMOS_LVDS_N) 1
-
+set ad_project_params(FPGA_BOARD) "zc706"
+set ad_project_params(FPGA_PART) "xczu9eg-ffvb1156-2-e"
